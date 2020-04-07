d438a13f4ff6e04f2dc667ba79f66f8a57cd0183
Arup Sarkar <arup.sarkar@kavayah.onmicrosoft.com>
2020-04-07 15:03:10 -0400

    Adding postgres conneciton pool

---
+++ src/main/java/com/ai/database/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package com.ai.database;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DatabaseConnection {
+
+	public BasicDataSource createConnectionPool() throws URISyntaxException, SQLException {
+		
+		BasicDataSource basicDataSource = new BasicDataSource();
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+		
+		
+		  if (dbUri.getUserInfo() != null) {
+			  basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+			  basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
+		  }
+		  basicDataSource.setDriverClassName("org.postgresql.Driver");
+		  basicDataSource.setUrl(dbUrl);
+		  basicDataSource.setInitialSize(1);
+			  
+		return basicDataSource;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
