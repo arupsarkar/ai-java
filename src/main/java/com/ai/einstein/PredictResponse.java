36df6044dc6545bd0554b47fd2dea3925213f1b4
Arup Sarkar <arup.sarkar@kavayah.onmicrosoft.com>
2020-04-07 00:33:40 -0400

    Added ignore props

--- src/main/java/com/ai/einstein/PredictResponse.java
+++ src/main/java/com/ai/einstein/PredictResponse.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PredictResponse {
 
 	@JsonProperty("message")
