c82c5823cff7f776b6ecb0299b1445ec68eca730
Arup Sarkar <arup.sarkar@kavayah.onmicrosoft.com>
2020-04-06 23:55:45 -0400

    few more spools

--- src/main/java/com/ai/http/AccessTokenRequest.java
+++ src/main/java/com/ai/http/AccessTokenRequest.java
@@ -1,6 +1,7 @@
 package com.ai.http;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
@@ -11,6 +12,7 @@ import com.ai.token.AccessToken;
 public class AccessTokenRequest extends Request{
 
 	private final String assertion;
+	private static String TAG = AccessTokenRequest.class.getName();
 	
 	public AccessTokenRequest(String assertion) {
 		super("");
@@ -22,15 +24,19 @@ public class AccessTokenRequest extends Request{
 	    Form form = new Form();
 	    form.param("assertion", assertion);
 	    form.param("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
-	    
+	    	    
 	    Entity<Form> entity = Entity.form(form);
 	    Response response = client.target(EINSTEIN_VISION_URL + "/v2/oauth2/token")
 	            .request()
 	            .post(entity);	    
-	    
+	      
+	    System.out.println( new Date() + TAG + " response : " + String.valueOf(response.getStatus()));
+	      
 	    if (!isSuccessful(response)) {
 	        throw new IOException("Error occurred while fetching Access Token " + response);
-	      }
+	    }else {
+	    	System.out.println( new Date() + TAG + " response success ");	    		    	
+	    }
 	    
 		return readResponseAs(response, AccessToken.class);
 		
