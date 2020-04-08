ca5bfb701d8ab72d8ed4be5f14553dab50ea7315
Arup Sarkar <arup.sarkar@kavayah.onmicrosoft.com>
2020-04-07 20:25:57 -0400

    Added sentiment analysis

--- src/main/java/com/ai/einstein/PredictSentimentResponse.java
+++ src/main/java/com/ai/einstein/PredictSentimentResponse.java
@@ -3,11 +3,19 @@ package com.ai.einstein;
 import java.io.IOException;
 import java.util.Date;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 import com.ai.http.Request;
 
 public class PredictSentimentResponse extends Request {
 	private String TAG = PredictSentimentResponse.class.getName();
 	private String token;
+	private final String FULL_EINSTEIN_LANG_URL = EINSTEIN_VISION_URL + "/v2/language/sentiment";
+	
 	public PredictSentimentResponse(String token) {
 		super(token);
 		this.token = getToken();
@@ -17,8 +25,27 @@ public class PredictSentimentResponse extends Request {
 	public PredictResponse submit() throws IOException {
 		
 		System.out.println(new Date() + " : " + TAG + " : Sentiment analysis - Start");
-		System.out.println(new Date() + " : " + TAG + " : Sentiment analysis - End");
-		return null;
+		Entity entity = null;
+		String text = "I like it";
+		String body = "{\"modelId\":\"CommunitySentiment\",\"document\":\"" + text + "\"}";
+		entity.json(body);
+		
+		Client client = ClientBuilder.newClient();
+		Response response = client.target(FULL_EINSTEIN_LANG_URL)
+							.request()
+							.header("Authorization", "Bearer " + getToken())
+							.post(entity);
+		
+	    if (!isSuccessful(response)) {
+		      throw new IOException("Error occurred while making prediction call " + response);
+		    }		
+		
+	    System.out.println(new Date() + " : " + TAG + " : Sentiment analysis - End " + response.getStatus());			
+		
+		return readResponseAs(response, PredictResponse.class);
+		
+
+
 	}
 
 }
